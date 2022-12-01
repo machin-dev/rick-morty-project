3f6b08debc8f39584595b74274c2048310a8dcbe
Michel Machin <michelma1988@gmail.com>
2022-12-01 09:05:51 -0500

    add species filter

--- src/componentes/Lista.js
+++ src/componentes/Lista.js
@@ -1,50 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Lista(props){    
     const {list,setList}=props; 
-    const [filter, setFilter]=useState([]);  
+    const [filter, SetFilter]=useState(null);
+      
     const reset= ()=>{
         console.log('resetando');
         setList("");
-    } 
-
-     const filtrar =(e) =>{
-      // setFilter([]);
-       console.log(e.target.value);
-       list.map((l)=>(      
-        
-         (l.species==e.target.value) ? ( 
-                           
-              setFilter([...filter,l])
-         ):(
-           console.log('--')
-         )     
-        ))
-        console.log(filter)
-     }
-
+        SetFilter(null);
+    }   
+     
+    useEffect(()=>(console.log(filter)))
     return(
         <div className="container-fluid text-light">
            <h3 className="text-light">Listado de personajes</h3> 
            <span className="text-primary text-decoration-underline " onClick={reset}>
             <button className="btn btn-secondary m-3" href="#">Atras</button>
            </span>
-           <label className="">Filtrar especie:
-           <select onChange={filtrar}>    
+           <label className="d-inline-flex align-items-center justify-content-center">Filtrar especie:
+           <select className="m-2" onChange={(e)=>(SetFilter(e.target.value))}>    
              <option name='' value=''></option>         
              <option name='human' value='Human'>Human</option>
              <option name='allien' value='Alien'>Alien</option>             
            </select>
+           <p className="text-secondary">{filter ? (
+                 <>{filter}</>
+           ):(
+            <>Todos los personajes</>
+           )}</p>
            </label>
            <div className="container-fluid">            
-            {list.map((l,index)=>(
-                
-                <div className="card d-inline-flex m-2 border b-2" max-width={50} key={index}>
+            {list.map((l,index)=>(   
+                <>
+                {l.species===filter || !filter ? (                    
+                    <div className="card d-inline-flex m-2 border b-2 bg-dark" max-width={50} key={index}>
                     <img src={l.image} title={l.name} alt="" />
                     <div className="card-header">
                         <p className="card-text fw-bold p-1 position-relative">
-                            Nombre: {l.name}
-                             <span className="badge bg-secondary position-absolute top-0 start-75 translate-middle ">{l.species}</span>
+                            Nombre: {l.name}                             
                         </p>
                         {
                             l.status==='Alive' ?
@@ -72,11 +65,16 @@ export default function Lista(props){
                               {l.episode.length}                               
                             </span>
                         </p>
-                        
+                        <span className="badge bg-dark position-absolute top-0 end-0 rounded-pill">{l.species}</span>
                     </div>    
-                </div>   
-               
-            ))} 
+                </div>
+                ):(
+                     console.log('not '+ filter)
+                )}
+                
+                </>        
+                    ))} 
+            
           </div>
         </div>
     );
